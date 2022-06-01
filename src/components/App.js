fd7cadd46750672d71b0167fc64c78301c246942
daeyeub <kdy9622@naver.com>
2022-06-01 23:52:52 +0900

    #2.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import AppRouter from "./Router";
+import { useState } from "react";
+import { auth } from "mybase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
   );
 }
 

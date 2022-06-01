b19571c8085e549efbc77a77b8ee6e874cad91cd
daeyeub <kdy9622@naver.com>
2022-06-02 00:19:56 +0900

    #2.1

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const { target: { name, value } } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
     return (
-        <div>Auth</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} name="email" type="text" placeholder="Email" required value={email} />
+                <input onChange={onChange} name="password" type="password" placeholder="Password" required value={password} />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
     )
 }
 
